d38a50debfd72e67ceef333f14cb1ead6eaec0ab
Felipe Quaresma Vieira <135285820+felipeqvieira@users.noreply.github.com>
2023-10-26 15:31:55 -0300

    Update matriz.c

--- matriz.c
+++ matriz.c
@@ -159,6 +159,113 @@ void prnMat (MatRow mat, int m, int n)
   printf(SEP_RES);
 }
 
+void multMatVetOtimizada(MatRow mat, Vetor v, int n, Vetor res){
+
+  for (int i = 0; i < n - n % UF_FACTOR; i += UF_FACTOR){
+    for (int j = 0; j < n; ++j)
+      res[i] = res[i] + mat[i * n + j] * v[j];
+    for (int j = 0; i < n; ++j)
+      res[i+1] = res[i+1] + mat[(i+1) * n + j] * v[j];
+    for (int j = 0; i < n; ++j)
+      res[i+2] = res[i+2] + mat[(i+2) * n + j] * v[j];
+    for (int j = 0; i < n; ++j)
+      res[i+3] = res[i+3] + mat[(i+3) * n + j] * v[j];
+    for (int j = 0; i < n; ++j)
+      res[i+4] = res[i+4] + mat[(i+4) * n + j] * v[j];
+    for (int j = 0; i < n; ++j)
+      res[i+5] = res[i+5] + mat[(i+5) * n + j] * v[j];
+    for (int j = 0; i < n; ++j)
+      res[i+6] = res[i+6] + mat[(i+6) * n + j] * v[j];
+    for (int j = 0; i < n; ++j)
+      res[i+7] = res[i+7] + mat[(i+7) * n + j] * v[j];
+    for (int j = 0; i < n; ++j)
+      res[i+8] = res[i+8] + mat[(i+8) * n + j] * v[j];
+    for (int j = 0; i < n; ++j)
+      res[i+9] = res[i+9] + mat[(i+9) * n + j] * v[j];
+    for (int j = 0; i < n; ++j)
+      res[i+10] = res[i+10] + mat[(i+10) * n + j] * v[j];
+    for (int j = 0; i < n; ++j)
+      res[i+11] = res[i+11] + mat[(i+11) * n + j] * v[j];
+    for (int j = 0; i < n; ++j)
+      res[i+12] = res[i+12] + mat[(i+12) * n + j] * v[j];
+    for (int j = 0; i < n; ++j)
+      res[i+13] = res[i+13] + mat[(i+13) * n + j] * v[j];
+    for (int j = 0; i < n; ++j)
+      res[i+14] = res[i+14] + mat[(i+14) * n + j] * v[j];
+    for (int j = 0; i < n; ++j)
+      res[i+15] = res[i+15] + mat[(i+15) * n + j] * v[j];
+    for (int j = 0; i < n; ++j)
+      res[i+16] = res[i+16] + mat[(i+16) * n + j] * v[j];
+    for (int j = 0; i < n; ++j)
+      res[i+17] = res[i+17] + mat[(i+17) * n + j] * v[j];
+    for (int j = 0; i < n; ++j)
+      res[i+18] = res[i+18] + mat[(i+18) * n + j] * v[j];
+    for (int j = 0; i < n; ++j)
+      res[i+19] = res[i+19] + mat[(i+19) * n + j] * v[j];
+    for (int j = 0; i < n; ++j)
+      res[i+20] = res[i+20] + mat[(i+20) * n + j] * v[j];
+    for (int j = 0; i < n; ++j)
+      res[i+21] = res[i+21] + mat[(i+21) * n + j] * v[j];
+    for (int j = 0; i < n; ++j)
+      res[i+22] = res[i+22] + mat[(i+22) * n + j] * v[j];
+    for (int j = 0; i < n; ++j)
+      res[i+23] = res[i+23] + mat[(i+23) * n + j] * v[j];
+    for (int j = 0; i < n; ++j)
+      res[i+24] = res[i+24] + mat[(i+24) * n + j] * v[j];
+    for (int j = 0; i < n; ++j)
+      res[i+25] = res[i+25] + mat[(i+25) * n + j] * v[j];
+    for (int j = 0; i < n; ++j)
+      res[i+26] = res[i+26] + mat[(i+26) * n + j] * v[j];
+    for (int j = 0; i < n; ++j)
+      res[i+27] = res[i+27] + mat[(i+27) * n + j] * v[j];
+    for (int j = 0; i < n; ++j)
+      res[i+28] = res[i+28] + mat[(i+28) * n + j] * v[j];
+    for (int j = 0; i < n; ++j)
+      res[i+29] = res[i+29] + mat[(i+29) * n + j] * v[j];
+    for (int j = 0; i < n; ++j)
+      res[i+30] = res[i+30] + mat[(i+30) * n + j] * v[j];
+    for (int j = 0; i < n; ++j)
+      res[i+31] = res[i+31] + mat[(i+31) * n + j] * v[j];
+  }
+
+  for(int i = n-n%UF_FACTOR; i < n; ++i)
+    for (int j = 0; j < n; ++j)
+      res[i] = res[i] + mat[i * n + j] * v[j];
+}
+
+void multMatMatOtimizada(MatRow A, MatRow B, int n, MatRow C){
+
+  for (int i = 0; i < n; ++i){
+    for (int j = 0; j < n-n%BK; j+= BK){
+      C[i*n+j] = C[i*n+(j+1)] = C[i*n+(j+2)] = C[i*n+(j+3)] = C[i*n+(j+4)] = C[i*n+(j+5)] = C[i*n+(j+6)] = C[i*n+(j+7)] = C[i*n+(j+8)] = C[i*n+(j+9)] = C[i*n+(j+10)] = C[i*n+(j+11)] = C[i*n+(j+12)] = C[i*n+(j+13)] = C[i*n+(j+14)] = C[i*n+(j+15)] = 0;
+      for (int k = 0; k < n; ++k){
+        C[i*n+j] += A[i*n+k] * B[k*n+j];
+        C[i*n+(j+1)] += A[i*n+k] * B[k*n+(j+1)];
+        C[i*n+(j+2)] += A[i*n+k] * B[k*n+(j+2)];
+        C[i*n+(j+3)] += A[i*n+k] * B[k*n+(j+3)];
+        C[i*n+(j+4)] += A[i*n+k] * B[k*n+(j+4)];
+        C[i*n+(j+5)] += A[i*n+k] * B[k*n+(j+5)];
+        C[i*n+(j+6)] += A[i*n+k] * B[k*n+(j+6)];
+        C[i*n+(j+7)] += A[i*n+k] * B[k*n+(j+7)];
+        C[i*n+(j+8)] += A[i*n+k] * B[k*n+(j+8)];
+        C[i*n+(j+9)] += A[i*n+k] * B[k*n+(j+9)];
+        C[i*n+(j+10)] += A[i*n+k] * B[k*n+(j+10)];
+        C[i*n+(j+11)] += A[i*n+k] * B[k*n+(j+11)];
+        C[i*n+(j+12)] += A[i*n+k] * B[k*n+(j+12)];
+        C[i*n+(j+13)] += A[i*n+k] * B[k*n+(j+13)];
+        C[i*n+(j+14)] += A[i*n+k] * B[k*n+(j+14)];
+        C[i*n+(j+15)] += A[i*n+k] * B[k*n+(j+15)];
+      }
+    }
+
+    for (int j = n-n%BK; j < n; ++j){
+      C[i*n+j] = 0;
+      for (int k = 0; k < n; ++k)
+        C[i*n+j] += A[i*n+k] * B[k*n+j];
+    } 
+  }
+}
+
 /**
  *  Funcao prnVetor:  Imprime o conteudo de vetor em stdout
  *  @param vet vetor com 'n' elementos
